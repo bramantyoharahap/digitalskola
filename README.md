deca9c41f0e1db860d3b8b015d1e10a5b33ebee2
Febriano Bramantyo <febriano.bramantyo@gmail.com>
2023-05-29 03:22:03 +0700

    update

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 2. From local direcotory of this repository (digitalskola), run command below in your terminal 
 
 ```bash
-cd project-1/env/src && python3 excel_report.py
+cd project-1 && source env/bin/activate
+cd env/src && python3 excel_report.py
 ```
--- project-1/env/src/operators/xlsx_report_plugin.py
+++ project-1/env/src/operators/xlsx_report_plugin.py
@@ -24,7 +24,7 @@ class ExcelReportPlugin():
 
     def run(self):
         df = self.read_input_file()
-        df_transform = self.transform(df)
+        df_transform = self.transform2(df)
         self.create_output_file(df_transform)
 
         wb = load_workbook(self.output_file)
@@ -37,6 +37,7 @@ class ExcelReportPlugin():
 
         self.barchart(wb.active, min_column, max_column, min_row, max_row)
         self.add_total(wb.active, max_column, max_row, min_row)
+        self.resolve_column_width(wb.active)
         self.save_file(wb)
         self.send_to_discord(webhook_url)
 
@@ -44,11 +45,21 @@ class ExcelReportPlugin():
         df = pd.read_excel(self.input_file)
         return df
     
-    def transform(self, df_transform):
+    def transform1(self, df_transform):
         df_transform = df_transform.pivot_table(index='Gender', 
                     columns='Product line', 
                     values='Total', 
                     aggfunc='sum').round()
+        
+        return df_transform
+    
+    def transform2(self, df_transform):
+        df_transform['Date'] = pd.to_datetime(df_transform["Date"]).dt.strftime('%Y-%m-%d')
+        df_transform = df_transform.pivot_table(index=['Gender','Date'], 
+                    columns='Product line', 
+                    values='Total', 
+                    aggfunc='sum').round()
+        
         return df_transform
         
     def create_output_file(self, df):
@@ -60,14 +71,14 @@ class ExcelReportPlugin():
         barchart = BarChart()
 
         data = Reference(workbook, 
-                         min_col=min_column+1,
+                         min_col=min_column+2,
                          max_col=max_column,
                          min_row=min_row,
                          max_row=max_row
                         )
 
         categories = Reference(workbook,
-                                min_col=min_column,
+                                min_col=min_column+1,
                                 max_col=min_column,
                                 min_row=min_row+1,
                                 max_row=max_row
@@ -75,27 +86,47 @@ class ExcelReportPlugin():
 
         barchart.add_data(data, titles_from_data=True)
         barchart.set_categories(categories)
+        barchart.gapWidth = 500
 
 
-        workbook.add_chart(barchart, 'B12')
+        workbook.add_chart(barchart, 'J5')
         barchart.title = 'Sales berdasarkan Produk'
         barchart.style = 2
+        barchart.height = 20
+        barchart.width = 150
         
     def add_total(self, workbook, max_column, max_row, min_row):
         alphabet = list(string.ascii_uppercase)
         alphabet_excel = alphabet[:max_column]
         #[A,B,C,D,E,F,G]
         for i in alphabet_excel:
-            if i != 'A':
+            if i != 'A' and i != 'B':
                 workbook[f'{i}{max_row+1}'] = f'=SUM({i}{min_row+1}:{i}{max_row})'
                 workbook[f'{i}{max_row+1}'].style = 'Currency'
 
         workbook[f'{alphabet_excel[0]}{max_row+1}'] = 'Total'
 
+        workbook[f'{alphabet_excel[1]}{max_row+1}'] = f'=SUM({alphabet_excel[2]}{max_row+1}:{alphabet_excel[7]}{max_row+1})'
+        workbook[f'{alphabet_excel[1]}{max_row+1}'].style = 'Currency'
+
         workbook['A1'] = 'Sales Report'
         workbook['A2'] = '2019'
         workbook['A1'].font = Font('Arial', bold=True, size=20)
         workbook['A2'].font = Font('Arial', bold=True, size=10)
+
+    def resolve_column_width(self, workbook):
+        for col in workbook.columns:
+            max_length = 0
+            column = col[0].column_letter # Get the column name
+            for cell in col:
+                try: # Necessary to avoid error on empty cells
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(str(cell.value))
+                except:
+                    pass
+            adjusted_width = (max_length + 2) * 0.9
+            workbook.column_dimensions[column].width = adjusted_width
+
         
     def save_file(self, wb):
         wb.save(self.output_file)
